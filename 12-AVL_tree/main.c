fa1a4b4550db574eb1d7627e58e1f043ecb3e183
Gabriel Rosin <39499969+gabao55@users.noreply.github.com>
2021-06-26 10:09:59 -0300

    Add files via upload

---
+++ 12-AVL_tree/main.c
@@ -0,0 +1,112 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include "avl.h"
+#define MAX 50
+
+int main() {
+    char cmd[MAX];
+    int keep_running = 1;
+    int number;
+    tree *T = NULL;
+    node *n;
+
+    while (keep_running) {
+        scanf("%s%*c", cmd);
+        if (strcmp(cmd, "inserir") == 0 || strcmp(cmd, "remover") == 0 || strcmp(cmd, "buscar") == 0 ||
+            strcmp(cmd, "sucessor") == 0 || strcmp(cmd, "predecessor") == 0)
+            scanf(" %d", &number);
+
+        if (strcmp(cmd, "criar") == 0) {
+            T = create(T);
+            if (!T)
+                exit(errno);
+        }
+
+        if (strcmp(cmd, "buscar") == 0) {
+            n = search(T->root, number);
+            if (!n)
+                printf("%d nao esta na arvore\n", number);
+            else
+                printf("%d esta na arvore\n", number);
+        }
+
+        if (strcmp(cmd, "inserir") == 0) {
+            keep_running = insert(T, number);
+            if (!keep_running) {
+                printf("memoria insuficiente");
+                keep_running = 1;
+            }
+        }
+
+        if (strcmp(cmd, "pre-ordem") == 0)
+            pre_order(T);
+
+        if (strcmp(cmd, "em-ordem") == 0)
+            in_order(T);
+
+        if (strcmp(cmd, "pos-ordem") == 0)
+            post_order(T);
+
+        if (strcmp(cmd, "sucessor") == 0) {
+            n = successor(T, number);
+            if (!n)
+                printf("nao ha sucessor de %d\n", number);
+            else
+                printf("sucessor de %d: %d\n", number, n->key);
+        }
+
+        if (strcmp(cmd, "predecessor") == 0) {
+            n = predecessor(T, number);
+            if (!n)
+                printf("nao ha predecessor de %d\n", number);
+            else
+                printf("predecessor de %d: %d\n", number, n->key);
+        }
+
+        if (strcmp(cmd, "remover") == 0)
+            delete(T, number);
+
+        if (strcmp(cmd, "avl?") == 0) {
+            count_bf(T->root);
+            keep_running = is_avl(T->root);
+            if (keep_running)
+                printf("sim\n");
+            else {
+                printf("nao\n");
+                keep_running = 1;
+            }
+        }
+
+        if (strcmp(cmd, "info") == 0) {
+            keep_running = nodes_number(T->root);
+            printf("nos: %d, ", keep_running);
+            keep_running = leaves_number(T->root);
+            printf("folhas: %d, ", keep_running);
+            keep_running = height(T->root) - 1;
+            if (keep_running < 0)
+                keep_running = 0;
+            printf("altura: %d\n", keep_running);
+            keep_running = 1;
+        }
+
+        if (strcmp(cmd, "caminho-mais-longo") == 0) {
+            if (!T->root)
+                printf("arvore vazia\n");
+            else {
+                printf("caminho mais longo e mais a direita: ");
+                longest_path(T->root);
+                printf("\n");
+            }
+        }
+
+        if (strcmp(cmd, "terminar") == 0) {
+            bst_free(T);
+            keep_running = 0;
+        }
+    }
+
+    return 0;
+}
+
